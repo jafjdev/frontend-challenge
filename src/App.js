811cfe01453ec2c2d1635667958b52bda7831d2c
José Cedeno <jafjdev@gmail.com>
2021-06-06 20:33:19 -0400

    feat(app):App js fetching offer list and directory/backdrop component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import DirectoryList from "ui/components/DirectoryList";
-
-const fetchOffersList = () => {
-    return [];
-};
+import DirectoryList from "./ui/components/DirectoryList/DirectoryList";
+import { getOffers } from "./services/directory.api";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Backdrop from "@material-ui/core/Backdrop";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function App() {
+    const fetchOffersList = async page => {
+        setLoading(true);
+        return getOffers(page).finally(() => setLoading(false));
+    };
+
+    const useStyles = makeStyles(theme => ({
+        backdrop: {
+            zIndex: theme.zIndex.drawer + 1,
+            color: "#fff",
+        },
+    }));
+    const classes = useStyles();
+    const [loading, setLoading] = useState(true);
     return (
         <>
-            <p class="py-4 text-lg text-center">Start here :) Good luck!</p>
-
-            <DirectoryList offerList={fetchOffersList()} />
+            <DirectoryList offerList={fetchOffersList} />
+            <Backdrop className={classes.backdrop} open={loading}>
+                <CircularProgress color="#a0aec0" size={80} />
+            </Backdrop>
         </>
     );
 }
