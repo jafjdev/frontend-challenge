e0399aa55735a59ed9a18be9c0cf4a1274010686
José Cedeno <jafjdev@gmail.com>
2021-06-06 20:18:08 -0400

    feat(DirectoryList): Directory list component created with material-ui table components.

---
+++ src/ui/components/DirectoryList/DirectoryList.js
@@ -0,0 +1,157 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Container } from "@material-ui/core";
+import EnhancedTableHead from "./EnhancedTableHead";
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === "desc"
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: "20px",
+        width: "100%",
+    },
+    paper: {
+        width: "100%",
+        marginBottom: theme.spacing(2),
+    },
+    table: {
+        width: "100%",
+    },
+    visuallyHidden: {
+        border: 0,
+        clip: "rect(0 0 0 0)",
+        height: 1,
+        margin: -1,
+        overflow: "hidden",
+        padding: 0,
+        position: "absolute",
+        top: 20,
+        width: 1,
+    },
+}));
+
+export default function DirectoryList({ offerList, ...props }) {
+    const [data, setData] = useState();
+    const classes = useStyles();
+    const [order, setOrder] = React.useState("asc");
+    const [orderBy, setOrderBy] = React.useState("calories");
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+
+    useEffect(() => {
+        offerList(1).then(response => {
+            setData(response.data);
+        });
+    }, []);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = event => {
+        if (event.target.checked) {
+            const newSelecteds = data.data.map(n => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+        offerList(page).then(response => {
+            setData(response.data);
+        });
+    };
+
+    return data ? (
+        <Container fixed>
+            <div className={classes.root}>
+                <Paper className={classes.paper}>
+                    <TableContainer>
+                        <Table
+                            className={classes.table}
+                            aria-labelledby="tableTitle"
+                            aria-label="enhanced table">
+                            <EnhancedTableHead
+                                classes={classes}
+                                numSelected={selected.length}
+                                order={order}
+                                orderBy={orderBy}
+                                onSelectAllClick={handleSelectAllClick}
+                                onRequestSort={handleRequestSort}
+                                rowCount={data.data.length}
+                            />
+                            <TableBody>
+                                {stableSort(
+                                    data.data,
+                                    getComparator(order, orderBy),
+                                ).map((row, index) => {
+                                    return (
+                                        <TableRow
+                                            hover
+                                            role="checkbox"
+                                            tabIndex={-1}
+                                            key={index}>
+                                            <TableCell align="left">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {row.dv_cashback}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {row.dv_address}
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[]}
+                        component="div"
+                        count={
+                            data.pages * process.env.REACT_APP_DEFAULT_ELEMENTS
+                        }
+                        rowsPerPage={process.env.REACT_APP_DEFAULT_ELEMENTS}
+                        page={page}
+                        onChangePage={handleChangePage}
+                    />
+                </Paper>
+            </div>
+        </Container>
+    ) : null;
+}
